46a9b37ce9dcfbf7f9a691a48f1fd348bf964f92
Robert Edmonds <edmonds@isc.org>
2012-03-01 15:30:06 -0500

    setup.py: make cython optional

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
 
-setup(
-    name = 'wdns',
-    ext_modules = [
-        Extension('wdns', ['wdns.pyx'],
-            libraries = ['wdns'],
-            library_dirs = ['../wdns/.libs'],
-            include_dirs = ['../wdns']
-        )
-    ],
-    py_modules = ['wdns_constants'],
-    cmdclass = {'build_ext': build_ext},
-)
+try:
+    from Cython.Distutils import build_ext
+    setup(
+        name = 'wdns',
+        ext_modules = [ Extension('wdns', ['wdns.pyx'], libraries = ['wdns']) ],
+        cmdclass = {'build_ext': build_ext},
+        py_modules = ['wdns_constants'],
+
+    )
+except ImportError:
+    setup(
+        name = 'wdns',
+        ext_modules = [ Extension('wdns', ['wdns.c'], libraries = ['wdns']) ],
+        py_modules = ['wdns_constants'],
+    )
