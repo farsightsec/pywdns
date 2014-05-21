farsightsec/pywdns

commit d04d3919caea70b89aa337d2243dab59259c2d32

wdns.pyx: added length check to domain_to_str

wdns.pyx
@@ -97,8 +97,13 @@ def is_subdomain(str py_name0, str py_name1):
 
 def domain_to_str(str src):
     cdef char dst[WDNS_PRESLEN_NAME]
-    wdns_domain_to_str(<uint8_t *> PyString_AsString(src), len(src), dst)
-    return PyString_FromString(dst)
+    cdef size_t sz
+
+    if len(src) >= WDNS_PRESLEN_NAME:
+        raise NameException
+
+    sz = wdns_domain_to_str(<uint8_t *> PyString_AsString(src), len(src), dst)
+    return PyString_FromStringAndSize(dst, sz)
 
 def str_to_rrtype(char *src):
     cdef uint16_t res

commit b890c00a490f3951b2bce683a101d8a159922f12

domain_to_str(): PyString_FromStringAndSize -> PyString_FromString
this fixes a regression introduced in d04d3919. wdns_domain_to_str()
does not return the number of bytes written to dst, it returns the
number of bytes read from src. dst is guaranteed to be NUL-terminated
after a call to wdns_domain_to_str(), so switch back to using
PyString_FromString() to convert dst to a python string.

wdns.pyx
@@ -207,7 +207,10 @@ def domain_to_str(str src):
         raise NameException, repr(src)
 
     sz = wdns_domain_to_str(<uint8_t *> PyString_AsString(src), len(src), dst)
-    return PyString_FromStringAndSize(dst, sz)
+    if sz != len(src):
+        raise NameException, repr(src)
+
+    return PyString_FromString(dst)
 
 def str_to_rrtype(char *src):
     """