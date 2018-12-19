a0a934ef8a05353f12011584d7cd418d621f3d6f
Jeff Murphy <jcmurphy@jeffmurphy.org>
2018-12-18 22:14:04 -0500

    always generate cythonized file and constants file

--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2009-2014 by Farsight Security, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 NAME = 'pywdns'
-VERSION = '0.6.0'
+VERSION = '0.9.0'
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'Python extension module for the wdns C library'
+URL = 'https://github.com/farsightsec/pywdns'
+AUTHOR = 'Farsight Security, Inc.'
+AUTHOR_EMAIL = 'software@farsightsecurity.com'
 
 from distutils.core import setup
 from distutils.extension import Extension
 import subprocess
 
 subprocess.check_call('./gen_pywdns_constants')
+
 
 def pkgconfig(*packages, **kw):
     flag_map = {
@@ -29,24 +49,17 @@ def pkgconfig(*packages, **kw):
             kw.setdefault(flag_map[flag], []).append(arg)
     return kw
 
-try:
-    from Cython.Distutils import build_ext
-    setup(
-        name = NAME,
-        version = VERSION,
-        ext_modules = [ Extension('wdns', ['wdns.pyx'], **pkgconfig('libwdns >= 0.6.0')) ],
-        cmdclass = {'build_ext': build_ext},
-        py_modules = ['wdns_constants'],
+from Cython.Distutils import build_ext
+setup(
+    name = NAME,
+    version = VERSION,
+    license = LICENSE,
+    description = DESCRIPTION,
+    url = URL,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    ext_modules = [ Extension('wdns', ['wdns.pyx'], **pkgconfig('libwdns >= 0.9.0')) ],
+    cmdclass = {'build_ext': build_ext},
+    py_modules = ['wdns_constants'],
 
-    )
-except ImportError:
-    import os
-    if os.path.isfile('wdns.c'):
-        setup(
-            name = NAME,
-            version = VERSION,
-            ext_modules = [ Extension('wdns', ['wdns.c'], **pkgconfig('libwdns >= 0.6.0')) ],
-            py_modules = ['wdns_constants'],
-        )
-    else:
-        raise
+)
