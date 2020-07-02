d64b8dcde0faef865a0425fcf43a05bc4e2d11e5
Allan LeSage <alesage@fsi.io>
2020-07-02 19:36:37 +0000

    Vanilla unittest assertions for clarity.

--- tests/test.py
+++ tests/test.py
@@ -44,46 +44,45 @@ class TestWDNS(unittest.TestCase):
     def test_domain_manipulation(self):
         domain = 'fsi.io'
         name = str_to_name(domain)
-        assert name == b'\x03fsi\x02io\x00', name
-        assert domain_to_str(name) == 'fsi.io.', domain_to_str(name)
+        self.assertEqual(b'\x03fsi\x02io\x00', name)
+        self.assertEqual('fsi.io.', domain_to_str(name))
         rname = reverse_name(name)
-        assert rname == b'\x02io\x03fsi\x00', rname
-        assert left_chop(name) == b'\x02io\x00', left_chop(name)
-        assert count_labels(name) == 2, count_labels(name)
-        assert is_subdomain(str_to_name('www.' + domain), name) is True
-        assert is_subdomain(name, str_to_name('www.' + domain)) is False
+        self.assertEqual(b'\x02io\x03fsi\x00', rname)
+        self.assertEqual(b'\x02io\x00', left_chop(name))
+        self.assertEqual(2, count_labels(name))
+        self.assertTrue(is_subdomain(str_to_name('www.' + domain), name))
+        self.assertFalse(is_subdomain(name, str_to_name('www.' + domain)))
 
     def test_opcode_conversions(self):
-        assert str_to_rrtype('A') == 1, str_to_rrtype('A')
-        assert rrtype_to_str(16) == 'TXT', rrtype_to_str(16)
+        self.assertEqual(1, str_to_rrtype('A'))
+        self.assertEqual('TXT', rrtype_to_str(16))
 
-        assert opcode_to_str(0) == 'QUERY', opcode_to_str(0)
+        self.assertEqual('QUERY', opcode_to_str(0))
         # libwdns doesnt have str_to_opcode
 
-        assert rcode_to_str(3) == 'NXDOMAIN', rcode_to_str(3)
-        assert str_to_rcode('NXDOMAIN') == 3, str_to_rcode('NXDOMAIN')
+        self.assertEqual('NXDOMAIN', rcode_to_str(3))
+        self.assertEqual(3, str_to_rcode('NXDOMAIN'))
 
-        assert rrclass_to_str(1) == 'IN', rrclass_to_str(1)
-        assert str_to_rrclass('IN') == 1, str_to_rrclass('IN')
+        self.assertEqual('IN', rrclass_to_str(1))
+        self.assertEqual(1, str_to_rrclass('IN'))
 
     def test_txt_record_creation(self):
-        assert rdata_to_str(
-            b'\x10text record data', TYPE_TXT, CLASS_IN) == \
-            '"text record data"', rdata_to_str(
-                b'\x10text record data', TYPE_TXT, CLASS_IN)
-        assert str_to_rdata('"text record data"', TYPE_TXT, CLASS_IN) == \
-            b'\x10text record data',\
-            str_to_rdata('"text record data"', TYPE_TXT, CLASS_IN)
+        self.assertEqual(
+            '"text record data"',
+            rdata_to_str(b'\x10text record data', TYPE_TXT, CLASS_IN))
+        self.assertEqual(
+            b'\x10text record data',
+            str_to_rdata('"text record data"', TYPE_TXT, CLASS_IN))
 
     def test_str_to_name_case(self):
         domain = 'FsI.iO'
         name = str_to_name_case(domain)
-        assert name == b'\x03FsI\x02iO\x00', name
+        self.assertEqual(b'\x03FsI\x02iO\x00', name)
 
     def test_len_name(self):
         domain = 'FsI.iO'
         name = str_to_name_case(domain)
-        assert len_name(name) == len(name)
+        self.assertEqual(len(name), len_name(name))
 
     def test_parse_pkt_query(self):
         a = '1bb00100000100000000000006676f6f676c6503636f6d0000010001'
@@ -92,8 +91,8 @@ class TestWDNS(unittest.TestCase):
         except:
             b = a.decode('hex')
         c = parse_message(b)
-        assert c.qr is False
-        assert str(c.sec[0][0]) == 'google.com. IN A', c.sec[0][0]
+        self.assertFalse(c.qr)
+        self.assertEqual('google.com. IN A', str(c.sec[0][0]))
 
     def test_parse_pkt_response(self):
         a = ('1bb08180000100010000000006676f6f676c6503636f6d0000010001c00c0001'
@@ -103,10 +102,11 @@ class TestWDNS(unittest.TestCase):
         except:
             b = a.decode('hex')
         c = parse_message(b)
-        assert c.qr is True
-        assert str(c.sec[0][0]) == 'google.com. IN A', c.sec[0][0]
-        assert str(c.sec[1][0]) == 'google.com. 299 IN A 172.217.3.110', \
-                                    c.sec[1][0]
+        self.assertTrue(c.qr)
+        self.assertEqual('google.com. IN A', str(c.sec[0][0]))
+        self.assertEqual(
+            'google.com. 299 IN A 172.217.3.110',
+            str(c.sec[1][0]))
 
 
 if __name__ == '__main__':
